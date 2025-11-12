10b0ae56c5b6f4992a85a2930d93a75171a1176e
Zexols <aidosixleb@gmail.com>
2025-11-12 22:51:20 +0500

    Done

--- data.js
+++ data.js
@@ -3,11 +3,12 @@ async function getWeather(){
   const city = document.getElementById("city").value.trim();
   const out = document.getElementById("out");
   if(!city) return;
-
+  if(out !== "active"){
+    out.classList.remove("active")
+  }
   const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`);
   const data = await res.json();
   if(data.cod !== 200) return out.innerText = "City not found";
-
   out.innerHTML = `
     <h3 class ="City">${data.name}</h3>
     <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png">
